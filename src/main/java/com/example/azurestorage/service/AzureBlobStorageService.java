df23657fd4d46eda7b93c888acb5afd936cd7839
Rodrigo Loayza <loayza.rs@pucp.edu.pe>
2022-05-20 12:41:32 -0500

    Cambio de dependencia

--- src/main/java/com/example/azurestorage/service/AzureBlobStorageService.java
+++ src/main/java/com/example/azurestorage/service/AzureBlobStorageService.java
@@ -1,11 +1,18 @@
 package com.example.azurestorage.service;
 
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.blob.*;
+import com.azure.core.util.Context;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.*;
+import com.azure.storage.blob.options.BlobParallelUploadOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.*;
 
 @Service
@@ -18,77 +25,83 @@ public class AzureBlobStorageService {
     @Value("${spring.cloud.azure.storage.blob.container-name}")
     private String containerName;
 
-    private CloudBlobContainer containerClient() {
-        try {
-            // Retrieve storage account from connection-string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.parse(connectStr);
-
-            // Create the blob client.
-            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-
-            // Get a reference to a container, must be lower case
-            CloudBlobContainer container = blobClient.getContainerReference(containerName);
-
-            // Create the container if it does not exist.
-            container.createIfNotExists();
-
-            // Create a permissions object
-            BlobContainerPermissions containerPermissions = new BlobContainerPermissions();
-            // Include public access in the permissions object
-            containerPermissions.setPublicAccess(BlobContainerPublicAccessType.BLOB);
-            // Set the permissions on the container
-            container.uploadPermissions(containerPermissions);
-
-            return container;
+    private BlobContainerClient containerClient() {
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .connectionString(connectStr)
+                .buildClient();
+        BlobContainerClient blobContainer = blobServiceClient.getBlobContainerClient(containerName); //asegurarse que este creado a no ser que deseemos crear un container
+        if (blobContainer.getAccessPolicy().getBlobAccessType() != PublicAccessType.BLOB) {
+            BlobSignedIdentifier identifier = new BlobSignedIdentifier()
+                    .setId("imagenes")
+                    .setAccessPolicy(new BlobAccessPolicy().setPermissions("r"));
+            try {
+                blobContainer.setAccessPolicy(PublicAccessType.BLOB, Collections.singletonList(identifier));
+                System.out.printf("Set Access Policy completed %n");
+                if (!blobContainer.exists()) {
+                    blobContainer.create();
+                }
+            } catch (UnsupportedOperationException error) {
+                System.out.printf("Set Access Policy completed %s%n", error);
+                return null;
+            }
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            return null;
-        }
+        return blobContainer;
     }
 
-    public ArrayList<ListBlobItem> listaBlobs() {
-        try {
-            ArrayList<ListBlobItem> listaBlobs = new ArrayList<>();
-            for (ListBlobItem blobItem : containerClient().listBlobs()) {
-                listaBlobs.add(blobItem);
-            }
-            return listaBlobs;
+    public ArrayList<BlobItem> listaBlobs() {
+        BlobContainerClient blobContainer = containerClient();
+        if (blobContainer != null) {
+            try {
+                ArrayList<BlobItem> listaBlobs = new ArrayList<>();
+                blobContainer.listBlobs().forEach(listaBlobs::add);
+                return listaBlobs;
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
             return null;
         }
-
     }
 
     public String subirArchivo(MultipartFile file, String name) {
+        BlobContainerClient blobContainer = containerClient();
+        if (blobContainer != null) {
+            /* Obtiene la extensión del archivo subido */
+            String fileName = file.getOriginalFilename();
+            assert fileName != null;
+            String fileExtension = fileName.substring(fileName.lastIndexOf("."));
 
-        String fileName = file.getOriginalFilename();
-        assert fileName != null;
-        String fileExtension = fileName.substring(fileName.lastIndexOf("."));
+            try {
+                BlobClient blobClient = blobContainer.getBlobClient(name + fileExtension);
+                Map<String, String> blobMetadata = Collections.singletonMap("myblobmetadata", "sample");
+                BlobHttpHeaders httpHeaders = new BlobHttpHeaders()
+                        .setContentDisposition("attachment")
+                        .setContentType("image/jpeg");
+                byte[] md5 = MessageDigest.getInstance("MD5").digest(file.getBytes());
+                BlobParallelUploadOptions options = new BlobParallelUploadOptions(file.getInputStream());
+                options.setComputeMd5(true);
+                options.setHeaders(httpHeaders);
+                options.setMetadata(blobMetadata);
+                blobClient.uploadWithResponse(options, null, Context.NONE);
+                return blobClient.getBlobUrl();
 
-        try {
-            CloudBlockBlob blob = containerClient().getBlockBlobReference(name + fileExtension);
-            // Create or overwrite the blob with contents from an uploaded file from frontend.
-            blob.upload(file.getInputStream(), file.getSize());
-            return blob.getUri().toString();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
             return null;
         }
     }
 
     public boolean borrarArchivoPorNombre(String name) {
-        try {
-            containerClient().getBlockBlobReference(name)
-                    .deleteIfExists();
+        BlobContainerClient blobContainer = containerClient();
+        if (blobContainer != null) {
+            blobContainer.getBlobClient(name).delete();
             return true;
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else {
             return false;
         }
     }
